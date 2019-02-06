89553c69f44826307a78690dfd51618b889f7276
Robert-Jan Granneman <robert-jan.granneman@42.nl>
2019-02-06 16:05:44 +0100

    Cleanup debug messages.

--- panic.lua
+++ panic.lua
@@ -1,9 +1,4 @@
 Zombies = { "zombie", "zombie", "zombie", "zombie" }
-ConstructionYards = {}
-PlayerDomes = {}
-DomeCount = 0
-DomeThreshold = 1
-NewPlayerDomes = {}
 
 RetailiateBlasphemy = function()
     powerproxy = Actor.Create("powerproxy.parabombs", false, { Owner = creeps })
@@ -13,13 +8,16 @@ end
 
 LetDogsOut = function()
     dog1 = Actor.Create("dog", true, { Location = wpDogTrees.Location, Owner = creeps })
+    dog1.Stance = "AttackAnything"
     dog1.Hunt()
     if not DogCabin.IsDead then
         dog2 = Actor.Create("dog", true, { Location = wpDogCabin.Location, Owner = creeps })
+        dog2.Stance = "AttackAnything"
         dog2.Hunt()
     end
     if not DogHouse.IsDead then
         dog3 = Actor.Create("dog", true, { Location = wpDogHouse.Location, Owner = creeps })
+        dog3.Stance = "AttackAnything"
         dog3.Hunt()
     end
 end
@@ -28,12 +26,14 @@ InitiateZombieApocalipse = function()
     Utils.Do(Zombies, function(zombie)
         local z = Actor.Create(zombie, true, { Location = wpZombiesSpawn.Location, Owner = creeps })
         z.Move(wpZombies.Location)
+        z.Stance = "AttackAnything"
         z.Hunt()
     end)
 end
 
 WakeUpAntInMine = function()
     local ant = Actor.Create("ant", true, { Location = wpMineAnt.Location, Owner = creeps })
+    ant.Stance = "AttackAnything"
     ant.Hunt()
 end
 
@@ -45,13 +45,13 @@ SendSniper = function()
     transport.LoadPassenger(a)
     
     Trigger.OnPassengerExited(transport, function(t, p)
-        Trigger.OnIdle(p, p.Hunt)
+        p.Stance = "AttackAnything"
     end)
 
 	transport.Paradrop(wpSummit.Location)
     
     Trigger.OnKilled(a, function()
-        Trigger.AfterDelay(DateTime.Seconds(5), function()
+        Trigger.AfterDelay(DateTime.Seconds(60), function()
             SendSniper()
         end)
     end)
@@ -62,46 +62,24 @@ WorldLoaded = function()
     players = Player.GetPlayers(function(player)
         return player.IsLocalPlayer or player.IsBot
     end)
-    Utils.Do(players, function(player)
-        PlayerDomes[player.InternalName] = 0
-    end)
-
-    Utils.Do(players, function(player)
-        Media.Debug(player.Name)
-    end)
-
-    Media.Debug("Start to panic... please.")
-
-    Trigger.AfterDelay(DateTime.Seconds(5), function()
-        Media.Debug("Still panicing?")
-    end)
 
     Trigger.OnKilled(Church, function()
-        Media.Debug("Thou shall not destroy religion.")
         RetailiateBlasphemy()
     end)
 
     Trigger.OnKilled(OilPump, function()
-        Media.Debug("Oil pump destroyed.")
         InitiateZombieApocalipse()
     end)
 
     Trigger.OnKilled(DogKeeper, function()
-        Media.Debug("Dog keeper killed.")
         LetDogsOut()
     end)
 
     Trigger.OnKilled(MineBarrel, function()
-    Media.Debug("Wakes up ant.")
-        WakeUpAntInMine()
+        Trigger.AfterDelay(DateTime.Seconds(1), WakeUpAntInMine)
     end)
 
     Trigger.OnKilled(Refugee, function()
         SendSniper()
     end)
-end
-
-ticked = 0
-Tick = function()
-    -- ticked = ticked + 1
 end
